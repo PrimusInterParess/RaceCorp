23601e43e8ee5bca7966f9a40ce3a2a190ee981f
Dani Borisov <diesonnekind@gmail.com>
2022-08-29 09:18:42 +0300

    inital-creation

---
+++ RaceCorp.Common/GlobalConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace RaceCorp.Common
+{
+    public static class GlobalConstants
+    {
+        public const string SystemName = "RaceCorp";
+
+        public const string AdministratorRoleName = "Administrator";
+    }
+}
---
+++ Web/RaceCorp.Web/Areas/Administration/Controllers/DashboardController.cs
@@ -0,0 +1,23 @@
+﻿namespace RaceCorp.Web.Areas.Administration.Controllers
+{
+    using RaceCorp.Services.Data;
+    using RaceCorp.Web.ViewModels.Administration.Dashboard;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    public class DashboardController : AdministrationController
+    {
+        private readonly ISettingsService settingsService;
+
+        public DashboardController(ISettingsService settingsService)
+        {
+            this.settingsService = settingsService;
+        }
+
+        public IActionResult Index()
+        {
+            var viewModel = new IndexViewModel { SettingsCount = this.settingsService.GetCount(), };
+            return this.View(viewModel);
+        }
+    }
+}
