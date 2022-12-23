75a79e379b78282165b29929a90b4767e88c7aeb
Дани Борисов DAniborisov <diesonnekind@gmail.com>
2022-12-23 10:18:15 +0200

    ordered-errorMessageClass

--- RaceCorp.Common/GlobalErrorMessages.cs
+++ RaceCorp.Common/GlobalErrorMessages.cs
@@ -1,48 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RaceCorp.Common
+﻿namespace RaceCorp.Common
 {
     public static class GlobalErrorMessages
     {
+        // formats
         public const string StringLengthError = "{0} should be between {2} and {1} characters!";
-
-        public const string PasswordConfirmPassowrdDontMatch = "The password and confirmation password do not match.";
-
-        public const string InvalidRaceId = "Invalid race";
-
-        public const string InvalidInputData = "Invalid Input data";
-
-        public const string TeamAlreadyExists = "Invalid team name!";
-
-        public const string AlreadyHaveCreatedTeam = "You have already created team!";
-
-        public const string InvalidRequest = "You made invalid request!";
-
-        public const string AlreadyRequested = "You have already requested to join the team";
-
-        public const string InvalidSearchTerms = "Invalid search terms. Max words count is two";
-
-        public const string TeamNoLongerExists = "Team no longer exists.Have a nice life!";
-
-        public const string InvalidTeam = "Invalid team!";
-
-        public const string UnauthorizedRequest = "Unauthorized request";
-
-        public const string AlreadyRequestedConnection = "Connection is already requested!";
-
-        public const string AlreadyConnected = "Already connected!";
-
         public const string AlreadyRegisteredForAnotherTrace = "Cannot register for this trace.You are already registered for {0} and it starts {1}";
-
-        public const string TeamDeleted = "Team is deleted!";
-
         public const string AlreadyHaveTeam = "You are already a member of {0}";
 
+        // pass
+        public const string PasswordConfirmPassowrdDontMatch = "The password and confirmation password do not match.";
+
+        // team
+        public const string TeamAlreadyExists = "Invalid team name!";
+        public const string AlreadyHaveCreatedTeam = "You have already created team!";
+        public const string AlreadyRequested = "You have already requested to join the team";
+        public const string TeamNoLongerExists = "Team no longer exists.Have a nice life!";
+        public const string InvalidTeam = "Invalid team!";
+        public const string TeamDeleted = "Team is deleted!";
+
+        // invalid
+        public const string InvalidRequest = "You made invalid request!";
+        public const string InvalidInputData = "Invalid Input data";
+        public const string InvalidRaceId = "Invalid race";
+        public const string InvalidSearchTerms = "Invalid search terms. Max words count is two";
         public const string NotExistingContent = "The content you are trying to reach does not exists";
 
+        // unauthorized
+        public const string UnauthorizedRequest = "Unauthorized request";
+
+        // connection
+        public const string AlreadyRequestedConnection = "Connection is already requested!";
+        public const string AlreadyConnected = "Already connected!";
     }
 }
